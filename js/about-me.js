d48ace14f37ca75e9dee99bc08d7b38af33d0836
MysticalGuest <1317148109@qq.com>
2020-07-21 19:37:55 +0800

    Site updated: 2020-07-21 19:37:50

--- js/about-me.js
+++ js/about-me.js
@@ -1,1 +1,22 @@
-$(function(){$.post("https://api.gushi.ci/all.json",{},function(t,i){var e="<blockquote>"+t.origin+'<br>"'+t.content+'"<br>–'+t.author+"</blockquote>";$("#poetry-container-time").append("<p>"+(new Date).Format("yyyy.MM.dd/hh:mm:ss")+"</p>"+e)})}),$(function(){$.getJSON("../json_data/record.json",function(t){$.each(t,function(t,i){var e='<li class="time-axis-item"><div class="time-axis-date">'+i.date+'<span></span></div><div class="time-axis-title">'+i.title+'</div><p class="time-axis-achievement">'+i.achievement+"</p></li>";$(".time-axis").append(e)})})});
+// author by removef
+// https://removeif.github.io/
+$(function () { // 获取一句诗
+    $.post("https://api.gushi.ci/all.json", {}, function (data, status) {
+        var htmlC = "<blockquote>" + data.origin + "<br>\"" + data.content + "\"<br>–" + data.author + "</blockquote>";
+        $("#poetry-container-time").append("<p>" + new Date().Format("yyyy.MM.dd/hh:mm:ss") + "</p>" + htmlC);
+    })
+});
+
+$(function () { // 获取记录数据
+    $.getJSON("../json_data/record.json", function (data) {
+        $.each(data, function (i, e) {
+            var html = '<li class="time-axis-item">' +
+                '<div class="time-axis-date">' + e.date + '<span></span></div>' +
+                '<div class="time-axis-title">' + e.title + '</div>' +
+                '<p class="time-axis-achievement">' + e.achievement + '</p>' +
+                '</li>';
+            $('.time-axis').append(html);
+        });
+    })
+});
+
